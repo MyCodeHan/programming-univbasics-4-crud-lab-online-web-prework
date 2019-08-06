dedaafefcaf5cf2715465221edc760ea02ae86b0
Hannah Harrington <hannahharrington@outlook.com>
2019-08-06 23:30:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -39,5 +39,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+  Family = Array ["Kornell", "Mya", "Gma","Papa", "GiGi"]
+  puts Family [0] = "Cornell"
 end
